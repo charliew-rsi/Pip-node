5de2602fac5d8e2edf2bac2c2adf97de7b66e31f
Charlie Wyman <charlie.wyman@ruralsource.com>
2018-10-16 13:53:49 -0400

    added uuid to submit article

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require("body-parser");
+var uuidv1 = require("uuid/v1");
 var fs = require("fs");
 
 var app = express();
@@ -18,23 +19,34 @@ app.post('/user/authenticate', function (req, res) {
     //placeholder until we move user authentication to remote service --CHW
 });
 
-app.post('/article/submit', function (req, res) {
+app.post('/article/submit', (req, res) => {
     var articles = require("./data/articles.json");
+    req.body["uuid"] = uuidv1();
     articles.push(req.body);
+    console.log("submitted article");
     fs.writeFile('data/articles.json', JSON.stringify(articles), (err) => {
         if (err) throw err;
       });    
  });
 
- app.get('/article/count', function(req, res){ 
-    var articles = require("./data/articles.json");
-    if (typeof articles === "undefined") {
-        res.sendStatus(500);
-    }
-    else {
-        res.json({count: articles.length});
-    }
- });
+app.get('/article/count', (req, res) => { 
+var articles = require("./data/articles.json");
+if (typeof articles === "undefined") {
+    res.sendStatus(500);
+}
+else {
+    res.json({count: articles.length});
+}
+});
+
+app.get('/articles/:pageNumber', (req, res) => { 
+    const articles = require("./data/articles.json");        
+    const index = req.params.pageNumber -1;
+    const threshold = index + 10 < articles.length ? index + 10 : articles.length;
+    const selectedArticles = articles.slice(index, threshold);
+    res.json({articles: selectedArticles});
+});
+
 
 
 var server = app.listen(8081, "127.0.0.1", function () {
